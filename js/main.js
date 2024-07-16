e03ebee947295c2cb79d4d5216bb07c9fc702183
deepalikhele <deepalikhele@gmail.com>
2024-07-17 01:23:54 +0530

    test

--- js/main.js
+++ js/main.js
@@ -100,3 +100,29 @@ function hideCard() {
     });
     overlay.style.display = 'none';
 }
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item1');
+const totalSlides = slides.length;
+
+function updateCarousel() {
+    const carouselInner = document.querySelector('.carousel-inner1');
+    carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateCarousel();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+}
+
+function autoScroll() {
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+}
+
+document.addEventListener('DOMContentLoaded', autoScroll);
