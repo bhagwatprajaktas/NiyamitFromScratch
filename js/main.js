3d0d83d1ca63d65d22d3df6c012481793157ab04
Prajakta <bhagwatprajaktas@gmail.com>
2024-07-18 15:57:18 +0530

    test

--- js/main.js
+++ js/main.js
@@ -10,12 +10,7 @@
         }, 1);
     };
     spinner();
-    
-    
-    // Initiate the wowjs
-    // new WOW().init();
-
-
+ 
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -26,21 +21,6 @@
     });
 
 
-    // Facts counter
-    // $('[data-toggle="counter-up"]').counterUp({
-    //     delay: 10,
-    //     time: 2000
-    // });
-
-
-    // Experience
-    // $('.experience').waypoint(function () {
-    //     $('.progress .progress-bar').each(function () {
-    //         $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    //     });
-    // }, {offset: '80%'});
-    
-    
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -54,35 +34,6 @@
         return false;
     });
 
-
-    // // Modal Video
-    // var $videoSrc;
-    // $('.btn-play').click(function () {
-    //     $videoSrc = $(this).data("src");
-    // });
-    // console.log($videoSrc);
-    // $('#videoModal').on('shown.bs.modal', function (e) {
-    //     $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    // })
-    // $('#videoModal').on('hide.bs.modal', function (e) {
-    //     $("#video").attr('src', $videoSrc);
-    // })
-
-
-    // Testimonial carousel
-    // $(".testimonial-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1000,
-    //     items: 1,
-    //     loop: true,
-    //     dots: false,
-    //     nav: true,
-    //     navText : [
-    //         '<i class="bi bi-arrow-left"></i>',
-    //         '<i class="bi bi-arrow-right"></i>'
-    //     ]
-    // });
-    
 })(jQuery);
 
 function showCard(cardId) {
